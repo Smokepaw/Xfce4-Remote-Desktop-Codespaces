9542faa3541c710bea41d82fede4a9ec5c43578e
Smokepaw <felixbrooksworth@tutamail.com>
2024-10-19 16:33:27 -0700

    Update start.sh
    - fixed file path

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the VNC server with the specified geometry and depth
-./noVNC/utils/launch.sh --vnc localhost:5909 --listen 6082
+/noVNC/utils/launch.sh --vnc localhost:5909 --listen 6082
