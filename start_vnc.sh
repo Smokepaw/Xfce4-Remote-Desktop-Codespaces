66e5f6e4bc77393008fdaf5dbaf2616440c373ef
Smokepaw <felixbrooksworth@tutamail.com>
2024-10-19 13:21:28 -0700

    Update start_vnc.sh
     - relocated misplaced lines from start.sh

--- start_vnc.sh
+++ start_vnc.sh
@@ -2,3 +2,7 @@
 
 # Start the VNC server with the specified geometry and depth
 vncserver :9 -geometry 1366x569 -depth 24 -log /dev/null &> /dev/null
+
+echo "Vnc server is ready, run 'start.sh' and click open in browser on the notification."
+echo "From there click vnc 'lite.html' and input the password"
+echo "Default password is 'UWUOWO'"
