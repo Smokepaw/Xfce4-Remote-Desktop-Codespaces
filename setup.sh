bfcbafd1c80bce1831c05b9122cb3a1bb4b827a3
Smokepaw <felixbrooksworth@tutamail.com>
2024-10-19 12:08:34 -0700

    Update setup.sh
    changed setup

--- setup.sh
+++ setup.sh
@@ -6,28 +6,10 @@ sudo apt update
 # Install Xfce4 and noVNC
 sudo apt install xfce4 xfce4-goodies -y
 
-# Set up VNC password (replace 'your_password' with a secure password)
-VNC_PASSWORD="UWUOWO"
-echo "$VNC_PASSWORD" | vncpasswd -f > ~/.vnc/passwd
-chmod 600 ~/.vnc/passwd
-
-# Create the VNC directory
-mkdir -p ~/.vnc
-
-# Create the xstartup file
-cat <<EOT >> ~/.vnc/xstartup
-#!/bin/sh
-xrdb $HOME/.Xresources
-startxfce4 &
-EOT
-
-# Make xstartup executable
-chmod +x ~/.vnc/xstartup
-
 # Clone noVNC repository
 git clone https://github.com/novnc/noVNC.git
 
-# Navigate to noVNC directory
-cd noVNC
+chmod +x setup2.sh
 
-echo "Setup complete. You can now start noVNC with the command './utils/launch.sh --vnc localhost:5901'."
+
+echo "Setup step 1 complete. You can now start step 2 with the command './setup2.sh'."
