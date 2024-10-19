a40b255920d19694129ba9f286ef21688d78035c
Smokepaw <felixbrooksworth@tutamail.com>
2024-10-19 13:21:01 -0700

    Update start.sh
    - relocating misplaced lines to start_vnc.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,3 @@
 
 # Start the VNC server with the specified geometry and depth
 ./noVNC/utils/launch.sh --vnc localhost:5909 --listen 6082
-
-echo "Vnc server is ready, run 'start.sh' and click open in browser on the notification."
-echo "From there click vnc 'lite.html' and input the password"
-echo "Default password is 'UWUOWO'"
