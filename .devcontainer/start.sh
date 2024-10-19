1c6e6f8148d1e2424e5a36f7caaefffd1fd9c79d
Smokepaw <felixbrooksworth@tutamail.com>
2024-10-19 16:45:27 -0700

    Update start.sh
    - fixed uncommitted issues

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the VNC server with the specified geometry and depth
-/noVNC/utils/launch.sh --vnc localhost:5909 --listen 6082
+/noVNC/utils/novnc_proxy --vnc localhost:5909 --listen 6082
